2e4358629518312cb00a464dc76aca80c2bf93a3
LUCIENE RODRIGUES DA SILVA <cyen@bol.com.br>
2023-10-10 18:14:10 -0300

    Add files via upload

---
+++ criptografia-frase/criptografiaFrase.md
@@ -0,0 +1,20 @@
+﻿![imagem_python](https://github.com/LucieneRodrigues/Cifra_de_Cesar2/assets/105310968/86c351a0-e133-4f8d-bf73-f40caf99598b)
+
+### Python
+
+### Esse codigo foi criado com o objetivo de treinar lógica, manipulação de valores em lista e conceito de regras de validação.
+
+#### Criptografa ou descriptografa frase com uma chave alfabetica.
+
+#### O exemplo  mostra o resultado ao digitar uma frase, digitar uma chave alfabetica com 26 caractere e escolher a opção "e" encriptografar.
+
+
+![criptografia_letras](https://github.com/LucieneRodrigues/Cifra_de_Cesar2/assets/105310968/77c8a714-03b4-4904-9478-7e39b0ee1f65)
+
+### Ao digitar a frase com criptografia e digitar a mesma chave, e escolher a opção "d"  (descriptografar) a frase será a mesma sem a criptografia.
+
+![descriptografa_letras](https://github.com/LucieneRodrigues/Cifra_de_Cesar2/assets/105310968/c113ea1e-256c-4cc1-b97b-9800d9553227)
+
+
+#### Para testar, instale um deditor de código (VS code, Python, Thonny) em sua máquina , copie o código cole no editor, salve em qualquer pasta em seu computador.
+#### Em seguida no editor, clique no botão Run, no console, digite uma frase, digite uma chave alfabetica desejada que contenha 26 caractere em seguida tecle enter.
---
+++ criptografia-frase/criptografiaFrase.py
@@ -0,0 +1,58 @@
+﻿
+
+      '''Criptografia: escreva uma frase, digite uma chave de 26 caractere alfabeticos, escolha "e/E" para criptografar ou
+digite uma frase criptografada, use a mesma chave de 26 caractere alfabeticos e escolha "d/D" para desecriptar'''
+
+
+print("\n")
+texto = str(input('Digite o texto desejado: ')).strip()
+chave = str(input('Digite a chave com 26 cacacterie alfabeticos: ')).strip().upper()
+while chave.isalpha() == False or len(chave)!=26:  
+    print('Chave inválida, digite uma chave alfabetica com 26 caractere: ')
+    chave = str(input('Digite a chave com 26 cacacterie alfabeticos: ')).strip().upper()
+modo = input('Escolha E para encriptar ou D para decriptar o texto: ').strip().upper()
+CARACTERES = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+caracteres = 'abcdefghijklmnopqrstuvwxyz'
+for caractere in texto: 
+    if caractere in caracteres: 
+        chave = chave.lower()
+CARACTERESV = ' '
+CARACTERESESP = ',.!?:;'
+
+convertido = list() 
+num = 0
+for caractere in texto:     
+    if modo == 'E':
+        if caractere in caracteres:
+            if caractere in chave: 
+                num = caracteres.find(caractere)                             
+                num = num                                      
+                convertido.append(chave[num])             
+        elif caractere in CARACTERES:
+            if caractere in chave: 
+                num = CARACTERES.find(caractere)                             
+                num = num                                    
+                convertido.append(chave[num])  
+        elif caractere in CARACTERESV:
+            convertido.append(CARACTERESV)
+        elif caractere in CARACTERESESP:
+            num = CARACTERESESP.find(caractere)
+            convertido.append(CARACTERESESP[num])
+
+    elif modo == 'D':
+        num = chave.find(caractere)                  
+        if caractere in CARACTERES: 
+            num = num                          
+            convertido.append(CARACTERES[num])        
+        elif caractere in caracteres:
+            num = num
+            convertido.append(caracteres[num])
+        elif caractere in CARACTERESV:
+            convertido.append(CARACTERESV)
+        elif caractere in CARACTERESESP:
+            num = CARACTERESESP.find(caractere)
+            convertido.append(CARACTERESESP[num])
+junta_caractere = ''.join(convertido) 
+    
+print(f'Resultado: {junta_caractere}')
+print("\n")
